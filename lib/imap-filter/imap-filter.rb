e127edc2bde2ba072b5f686f02280b15527ab962
Fred Mitchell <fred.mitchell@gmx.de>
2016-09-18 16:41:43 +0200

    verbose stuff

--- lib/imap-filter/imap-filter.rb
+++ lib/imap-filter/imap-filter.rb
@@ -4,21 +4,26 @@ include ImapFilter::DSL
 
 module ImapFilter
   module Functionality
+    def self.show_imap_plan
+      puts '====== Accounts'.light_yellow
+      _accounts.each do |name, account|
+        print " #{name}: ".light_green
+        print account.to_s.light_blue
+        puts
+      end
+      puts '====== Filters'.light_yellow
+      _filters.each do |name, filter|
+        print " #{name}: ".light_green
+        print filter.to_s.light_blue
+        puts          
+      end
+    end
+    
     def self.run_filters filters
       unless _options[:verbose] < 1
-        puts '====== Accounts'.light_yellow
-        _accounts.each do |name, account|
-          print " #{name}: ".light_green
-          print account.to_s.light_blue
-          puts
-        end
-        puts '====== Filters'.light_yellow
-        _filters.each do |name, filter|
-          print " #{name}: ".light_green
-          print filter.to_s.light_blue
-          puts          
-        end
+        show_imap_plan
       end
+      
     end
   end
 end
