3a06b2d5a6057c657afe08904020f22d47206562
Fred Mitchell <fred.mitchell@gmx.de>
2016-09-18 19:26:04 +0200

    logins work

--- lib/imap-filter/imap-filter.rb
+++ lib/imap-filter/imap-filter.rb
@@ -18,28 +18,29 @@ module ImapFilter
         puts          
       end
     end
-
-    def self.test_imap_accounts
-      puts '====== Testing Accounts'.light_yellow
+    
+    def self.login_imap_accounts test: false
+      puts "====== #{test ? 'Test' : 'Login'} Accounts".light_yellow
       _accounts.each do |name, account|
         print "  Testing #{name}...".light_white
         begin
           account._open_connection
-          #account._close_connection
           puts "SUCCESS".light_green
         rescue => e
-          puts "FAILED: #{e}".light_red          
+          puts "FAILED: #{e}".light_red
+          exit unless test
         end
       end
     end
 
     def self.execute_filters
+      login_imap_accounts
     end
     
     def self.run_filters filters
       show_imap_plan unless _options[:verbose] < 1
       if _options[:test]
-        test_imap_accounts
+        login_imap_accounts test: true
       else
         execute_filters
       end
