5874abb79cbd02f3be610ba3e528e41a9d0a532a
Fred Mitchell <fred.mitchell@gmx.de>
2016-09-18 11:38:40 +0200

    removing sensitive data

--- lib/imap-filter/cli.rb
+++ lib/imap-filter/cli.rb
@@ -3,6 +3,10 @@ require 'imap-filter'
 module ImapFilter
   module Cli
     class Main < Thor
+      class_option :verbose, type: :numeric, banner: '[1|2|3|4]', aliases: '-v', default: 0
+      def run(script = ENV['IMAPF_IMAP_FILE'] || 'default.imap')
+        
+      end
     end
   end
 end
