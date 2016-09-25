331f9e4effbca1c434103a3fd3265dd45334e0e1
Fred Mitchell <fred.mitchell@gmx.de>
2016-09-25 18:12:50 +0200

    documentation

--- lib/imap-filter/cli.rb
+++ lib/imap-filter/cli.rb
@@ -7,7 +7,7 @@ module ImapFilter
   module Cli
     class Main < Thor
       class_option :verbose, type: :numeric, banner: '[1|2|3|4]', aliases: '-v', default: 0
-      @@default_script = ENV['IMAPF_IMAP_FILE'] || 'default.imap' 
+      @@default_script = ENV['IMAPF_IMAP_FILE'] || 'default.imap'
       
       desc 'filter [script]', "Run the powerplay script. Default #{@@default_script}"
       option :dryrun, type: :boolean, aliases: '-u', desc: "Dry run, do not actually execute."
